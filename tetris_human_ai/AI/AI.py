1c715caed6a8a266e9c40f8ac42c750d6941c864
Justin Martin <professional@justinm.me>
2020-10-04 10:55:40 -0400

    Made some changes I think

--- tetris_human_ai/AI/AI.py
+++ tetris_human_ai/AI/AI.py
@@ -32,9 +32,9 @@ class BigBrain:
     def big_thonk(self, state: GameState) -> List[pygame.event.Event]:
         current_piece, next_piece, grid = state
         smallest_hole = ((0, 0), 11)
-        continuous_hole = ((0, 0), 0)
+        continuous_hole = (0, 0)
         for i, row in enumerate(reversed(grid)):
-            continuous_hole = ((0, i), 0)
+            continuous_hole = (0, 0)
             if (0, 0, 0) in row:
                 for j, cell in enumerate(row):
                     if cell == (0, 0, 0):
@@ -42,62 +42,62 @@ class BigBrain:
                     else:
                         if continuous_hole[1] > 0 and continuous_hole[1] < smallest_hole[1]:
                             smallest_hole = continuous_hole
-                        continuous_hole = ((j, i), 0)
+                        continuous_hole = (j+1, 0)
 
-        size_to_rotation = self._orientation_to_size(current_piece)
-        actions_to_take: List[pygame.event] = []
-        simulated_shape = copy(current_piece)
-        if continuous_hole[1] in size_to_rotation:
-            rotation_goal = first(size_to_rotation[continuous_hole[1]])
-            while simulated_shape.rotation != rotation_goal:
-                simulated_shape.rotation = simulated_shape.rotation + 1 % 4
-                actions_to_take.append(_rotate())  # Add Rotation Action
+                size_to_rotation = self._orientation_to_size(current_piece)
+                actions_to_take: List[pygame.event] = []
+                simulated_shape = copy(current_piece)
+                if continuous_hole[1] in size_to_rotation:
+                    rotation_goal = first(size_to_rotation[continuous_hole[1]])
+                    while simulated_shape.rotation != rotation_goal:
+                        simulated_shape.rotation = simulated_shape.rotation + 1 % 4
+                        actions_to_take.append(_rotate())  # Add Rotation Action
 
-            simulated_x = min([pos_x for pos_x, _ in convert_shape_format(simulated_shape)])
+                    simulated_x = min([pos_x for pos_x, _ in convert_shape_format(simulated_shape)])
 
-            while simulated_x != continuous_hole[0][0]:
-                if simulated_x < continuous_hole[0][0]:
-                    simulated_x = simulated_x + 1
-                    actions_to_take.append(_move_right())  # Add Move Right Event
+                    while simulated_x != continuous_hole[0]:
+                        if simulated_x < continuous_hole[0]:
+                            simulated_x = simulated_x + 1
+                            actions_to_take.append(_move_right())  # Add Move Right Event
+                        else:
+                            simulated_x = simulated_x - 1
+                            actions_to_take.append(_move_left())  # Add Move Left Event
+
+                            # We should maybe move down but that seems like bonus points when we could
+                            # Let the game do that for us :)
+
+                            return actions_to_take
+
                 else:
-                    simulated_x = simulated_x - 1
-                    actions_to_take.append(_move_left())  # Add Move Left Event
+                    for size in size_to_rotation:
+                        if continuous_hole[1] > size:
+                            can_fit = True
+                            break
+                        else:
+                            can_fit = False
+                    if can_fit:
+                        largest_size = max(size_to_rotation.keys())
+                        rotation_goal = first(size_to_rotation[largest_size])
+                        while simulated_shape.rotation != rotation_goal:
+                            simulated_shape.rotation = simulated_shape.rotation + 1 % 4
+                            actions_to_take.append(_rotate())  # Add Rotation Action
 
-                    # We should maybe move down but that seems like bonus points when we could
-                    # Let the game do that for us :)
+                        simulated_x = min([pos_x for pos_x, _ in convert_shape_format(simulated_shape)])
 
-                    return actions_to_take
+                        while simulated_x != continuous_hole[0]:
+                            if simulated_x < continuous_hole[0]:
+                                simulated_x = simulated_x + 1
+                                actions_to_take.append(_move_right())  # Add Move Right Event
+                            else:
+                                simulated_x = simulated_x - 1
+                                actions_to_take.append(_move_left())  # Add Move Left Event
 
-        else:
-            for size in size_to_rotation:
-                if continuous_hole[1] > size:
-                    can_fit = True
-                    break
-                else:
-                    can_fit = False
-            if can_fit:
-                largest_size = max(size_to_rotation.keys())
-                rotation_goal = first(size_to_rotation[largest_size])
-                while simulated_shape.rotation != rotation_goal:
-                    simulated_shape.rotation = simulated_shape.rotation + 1 % 4
-                    actions_to_take.append(_rotate())  # Add Rotation Action
+                        # We should maybe move down but that seems like bonus points when we could
+                        # Let the game do that for us :)
 
-                simulated_x = min([pos_x for pos_x, _ in convert_shape_format(simulated_shape)])
-
-                while simulated_x != continuous_hole[0][0]:
-                    if simulated_x < continuous_hole[0][0]:
-                        simulated_x = simulated_x + 1
-                        actions_to_take.append(_move_right())  # Add Move Right Event
+                        return actions_to_take
                     else:
-                        simulated_x = simulated_x - 1
-                        actions_to_take.append(_move_left())  # Add Move Left Event
-
-                # We should maybe move down but that seems like bonus points when we could
-                # Let the game do that for us :)
-
-                return actions_to_take
-            else:
-                raise RuntimeError("No Solution For Current State (Fix it so it finds a solution dumbass)")
+                        raise RuntimeError("No Solution For Current State (Fix it so it finds a solution dumbass)")
 
     def _orientation_to_size(self, piece: "Piece") -> ImmutableSetMultiDict[int, int]:
         rotation_to_size: List[Tuple[int, int]] = []
